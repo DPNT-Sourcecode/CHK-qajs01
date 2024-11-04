f6cb61ca0e7b87a3963a59dd0519a4df590959e6
sbx_user1051 <sbx_user1051@169.254.37.177>
2024-11-04 00:29:33 +0000

    Mon Nov 04 00:29:33 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,12 +6,15 @@ def checkout(skus):
     sku_set = {'A', 'B', 'C', 'D', 'E'}
     if not sku_set.issuperset(skus):
         return -1
+    basket = {}
+    
     checkout_value = skus.count('A') * 50 - skus.count('A') // 3 * 20 \
         + skus.count('B') * 30 - skus.count('B') // 2 * 15 \
         + skus.count('C') * 20 \
         + skus.count('D') * 15 \
         + skus.count('E') * 40 - min(skus.count('E') // 2, skus.count('B')) * 30
     return checkout_value
+
 
 
 
