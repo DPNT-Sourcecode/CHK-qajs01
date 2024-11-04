2bee5da81d1a04365de1db91e913ebb6299d5013
sbx_user1051 <sbx_user1051@169.254.37.177>
2024-11-04 00:16:22 +0000

    Mon Nov 04 00:16:22 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,8 +3,12 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    sku_set = {"A", "B", "C", "D"}
+    if !sku_set.issuperset(skus):
+        return -1
     checkout_value = skus.count('A') * 50 - skus.count('A') // 3 * 20 \
         + skus.count('B') * 30 - skus.count('B') // 2 * 15 \
         + skus.count('C') * 20 \
         + skus.count('D') * 15
     return checkout_value
+
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -6,3 +6,5 @@ class TestChk():
         assert checkout_solution.checkout("ABCD") == 115
         assert checkout_solution.checkout("AAAAB") == 210
         assert checkout_solution.checkout("BBBC") == 95
+        assert checkout_solution.checkout("DE") == 95
+
