e115ccc4736913dee1ee3921998dad16eca4be3f
sbx_user1051 <sbx_user1051@169.254.37.177>
2024-11-04 01:30:15 +0000

    Mon Nov 04 01:30:15 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -8,17 +8,19 @@ class TestChk():
         assert checkout_solution.checkout("") == 0
         # Basic
         assert checkout_solution.checkout("ABCD") == 115
-        # 
+        # Multi buy
         assert checkout_solution.checkout("AAAAB") == 210
         assert checkout_solution.checkout("AAAAAAB") == 280
         assert checkout_solution.checkout("BBBC") == 95
+        assert checkout_solution.checkout("NNNM") == 120
+        assert checkout_solution.checkout("VVVVV") == 220
+        # Cross bundle
         assert checkout_solution.checkout("EE") == 80
         assert checkout_solution.checkout("EEEBB") == 150
         assert checkout_solution.checkout("EEEBBB") == 165
+        # Single bundle
         assert checkout_solution.checkout("AFFFFF") == 90
-        assert checkout_solution.checkout("NNNM") == 120
         assert checkout_solution.checkout("UUUUU") == 160
-        assert checkout_solution.checkout("VVVVV") == 220
         # New price
         assert checkout_solution.checkout("KKK") == 190
         # Group buy
@@ -27,6 +29,7 @@ class TestChk():
         assert checkout_solution.checkout("ST") == 40
         assert checkout_solution.checkout("ZX") == 38
         assert checkout_solution.checkout("ZZZZ") == 66
+
 
 
 
