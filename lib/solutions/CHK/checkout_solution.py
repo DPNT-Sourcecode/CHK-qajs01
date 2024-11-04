79a097053942d0b26db7aa9fffd54714de94be8e
sbx_user1051 <sbx_user1051@169.254.37.177>
2024-11-04 00:23:33 +0000

    Mon Nov 04 00:23:33 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,8 +10,9 @@ def checkout(skus):
         + skus.count('B') * 30 - skus.count('B') // 2 * 15 \
         + skus.count('C') * 20 \
         + skus.count('D') * 15 \
-        + skus.count('E') * 40 - 
+        + skus.count('E') * 40 - min(skus.count('E') // 2, skus.count('B')) * 30
     return checkout_value
+
 
 
 
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -3,11 +3,14 @@ from solutions.CHK import checkout_solution
 
 class TestChk():
     def test_chk(self):
+        assert checkout_solution.checkout("Da") == -1
+        assert checkout_solution.checkout("") == 0
         assert checkout_solution.checkout("ABCD") == 115
         assert checkout_solution.checkout("AAAAB") == 210
         assert checkout_solution.checkout("BBBC") == 95
-        assert checkout_solution.checkout("Da") == -1
-        assert checkout_solution.checkout("") == 0
+        assert checkout_solution.checkout("EE") == 80
+        assert checkout_solution.checkout("EEEBB") == 150
+
 
 
 
