8171bd0916df4921ee51bcf009ad58215b945b39
sbx_user1051 <sbx_user1051@169.254.37.177>
2024-11-04 00:07:22 +0000

    Mon Nov 04 00:07:22 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,7 +3,11 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    checkout_value = skus.count('A') * 50 + skus.count('B') * 30 + skus.count('C') * 20 + skus.count('D') * 15
+    checkout_value = skus.count('A') * 50 - skus.count('A') // 3 * 20 \
+        + skus.count('B') * 30 - skus.count('B') // 2 * 15 \
+        + skus.count('C') * 20 \
+        + skus.count('D') * 15
     return checkout_value
+
 
 
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -4,4 +4,7 @@ from solutions.CHK import checkout_solution
 class TestChk():
     def test_chk(self):
         assert checkout_solution.checkout("ABCD") == 115
+        assert checkout_solution.checkout("AAAAB") == 210
+        assert checkout_solution.checkout("BBBC") == 95
+
 
