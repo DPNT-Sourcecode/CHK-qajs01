1a6a30a8adccc84a745f2c58ff31004ef1a3a2d0
sbx_user1051 <sbx_user1051@169.254.37.177>
2024-11-04 00:49:10 +0000

    Mon Nov 04 00:49:10 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,12 +1,32 @@
 # Set sku and price list
-sku_set = {'A', 'B', 'C', 'D', 'E', 'F'}
+sku_set = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}
 prices = {
     'A': 50,
     'B': 30,
     'C': 20,
     'D': 15,
     'E': 40,
-    'F': 10
+    'F': 10,
+    'G': 20,
+    'H': 10,
+    'I': 35,
+    'J': 60,
+    'K': 80,
+    'L': 90,
+    'M': 15,
+    'N': 40,
+    'O': 10,
+    'P': 50,
+    'Q': 30,
+    'R': 50': 3R get one Q free      |
+    'S': 30,
+    'T': 20,
+    'U': 40': 3U get one U free      |
+    'V': 50': 2V for 90, 3V for 130  |
+    'W': 20,
+    'X': 90,
+    'Y': 10,
+    'Z': 50
 }
 
 # noinspection PyUnusedLocal
@@ -39,3 +59,4 @@ def checkout(skus):
     for item in basket:
         checkout_value += basket[item] * prices[item]
     return checkout_value
+
